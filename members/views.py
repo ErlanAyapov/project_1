29268a1381a5ddf55cc70fcbffbdd3f475899348
ErlanAyapov <simsiti0009ggg@gmail.com>
2021-11-17 00:53:56 +0600

    fixing bugs

--- members/views.py
+++ members/views.py
@@ -103,7 +103,8 @@ class UserProfile(DetailView):
 	def get_context_data(self, **kwargs):
 		context = super(UserProfile, self).get_context_data(**kwargs)
 		context['last_progress'] = UserProgress.objects.all()
-		context['user_profile'] = UserPicture.objects.order_by('-pk')[0]
+		context['user_profile'] = UserPicture.objects.order_by('-pk')
+		# context['request_user'] = User.user.id
 
 		return context
 
